c02428865b0106e84795c5b92e855d3821ee4411
IcaroPCarpes <icaroprestescarpes@gmail.com>
2020-10-08 14:49:43 -0300

    Primeiro Commit

---
+++ html/header.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="../css/header.css">
+    </head>
+    <body>
+        <header>
+            <div id='header'>
+                <div id='menusuperior'>
+                    <button id='botao1'>Home</button>
+                    <button id='botao2'>Logoff</button>
+                </div>
+                <div id='titulo'> <p>Mecânica Oliveira</p>  </div>
+
+            </div>
+        </header>
+    </body>
+
+</html>
+
+
---
+++ html/home.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+        <title>
+            Home
+        </title>
+</head>
+<body>
+    <div id='tudo'>
+        
+    
+    </div>
+</body>
+    
+</html>
